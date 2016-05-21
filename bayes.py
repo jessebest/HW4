10506f716ab85cc5bd4c0f813ebd794fde32bf38
shangjiang <shangjiang@dhcp-10-105-109-254.wireless.northwestern.private>
2016-05-20 19:44:59 -0500

    bayes.py cross-validation

--- bayes.py
+++ bayes.py
@@ -8,44 +8,39 @@ import math, os, pickle, re
 
 
 class Bayes_Classifier:
-    def __init__(self):
+    def __init__(self, pos_dict = None, neg_dict = None, pos_total = 0, neg_total = 0, pos_doc = 0, neg_doc = 0):
 
         """This method initializes and trains the Naive Bayes Sentiment Classifier.  If a
             cache of a trained classifier has been stored, it loads this cache.  Otherwise,
             the system will proceed through training.  After running this method, the classifier
             is ready to classify input text."""
-        self.pos_dict = {}
-        self.neg_dict = {}
-        self.pos_total = 0
-        self.neg_total = 0
-        self.pos_doc = 0
-        self.neg_doc = 0
-
-        if os.path.isfile("Positive"):
-            self.pos_dict = self.load("Positive")
-        if os.path.isfile("Negative"):
-            self.neg_dict = self.load("Negative")
-        if os.path.isfile("pos_doc"):
-            self.pos_doc = self.load("pos_doc")
-        if os.path.isfile("neg_doc"):
-            self.neg_doc = self.load("neg_doc")
-        if os.path.isfile("pos_total"):
-            self.pos_total = self.load("pos_total")
-        if os.path.isfile("neg_total"):
-            self.neg_total = self.load("neg_total")
-
+        if pos_dict:
+            self.pos_dict = self.load(pos_dict)
         else:
-            self.train()
+            self.pos_dict = {}
+        if neg_dict:
+            self.neg_dict = self.load(neg_dict)
+        else:
+            self.neg_dict = {}
 
-    def train(self):
+        self.pos_total = pos_total
+        self.neg_total = neg_total
+        self.pos_doc = pos_doc
+        self.neg_doc = neg_doc
+        
+
+    def train(self,trainingset = "movies_reviews/", traininglist = None):
         """Trains the Naive Bayes Sentiment Classifier."""
-        Filelist = []
-        for FileObj in os.walk("movies_reviews/"):
-            Filelist = FileObj[2]
-            break
+        if not traininglist:
+            Filelist = []
+            for FileObj in os.walk(trainingset):
+                Filelist = FileObj[2]
+                break
+        else:
+            Filelist = traininglist
 
         for path in Filelist:
-            token_list = self.tokenize(self.loadFile("movies_reviews/" + path))
+            token_list = self.tokenize(self.loadFile(trainingset + path))
             star = path[7]
 
             if star == '1':
@@ -73,12 +68,78 @@ class Bayes_Classifier:
 
 
 
-        self.save(self.pos_dict, "Positive")
-        self.save(self.neg_dict, "Negative")
-        self.save(self.pos_doc, "pos_doc")
-        self.save(self.neg_doc, "neg_doc")
-        self.save(self.pos_total, "pos_total")
-        self.save(self.neg_total, "neg_total")
+        # self.save(self.pos_dict, "Positive")
+        # self.save(self.neg_dict, "Negative")
+        # self.save(self.pos_doc, "pos_doc")
+        # self.save(self.neg_doc, "neg_doc")
+        # self.save(self.pos_total, "pos_total")
+        # self.save(self.neg_total, "neg_total")
+
+    def ten_cv(self, trainingset = "movies_reviews/"):
+        Filelist = []
+
+        for FileObj in os.walk(trainingset):
+            Filelist = FileObj[2]
+            break
+        
+        print len(Filelist)
+
+        random.shuffle(Filelist)
+
+        print len(Filelist)
+
+        averageAccuracy = 0
+        averagePrecision = 0
+        averageRecall = 0
+        averageF1 = 0
+
+        for i in range(0,10):
+            validationset = Filelist[i * (len(Filelist) / 10) : (i + 1) * (len(Filelist) / 10)]
+            traininglist = []
+            
+            for f in Filelist:
+                if f not in validationset:
+                    traininglist.append(f)
+            
+            self.train(traininglist = traininglist)
+            true_pos = 0
+            true_neg = 0
+            false_pos = 0
+            false_neg = 0
+            for f in validationset:
+                text = self.loadFile(trainingset + f)
+                guess = self.classify(text)
+                if guess == "Positive" and f[7] == "5":
+                    true_pos += 1
+                elif guess == "Positive" and f[7] == "1":
+                    false_pos += 1
+                elif guess == "Negative" and f[7] == "1":
+                    true_neg += 1
+                elif guess == "Negative" and f[7] == "5":
+                    false_neg += 1
+                else:
+                    print "there is something wrong"
+                    
+            accuracy = (true_pos + true_neg) / float(true_pos + true_neg + false_pos + false_neg)
+            precision = true_pos / float(true_pos + false_pos)
+            recall = true_pos / float(true_pos + false_neg)
+            f1 = (2 * precision * recall) / float(precision + recall)
+            
+            averageAccuracy += accuracy
+            averagePrecision += precision
+            averageRecall += recall
+            averageF1 += f1
+            
+        averageAccuracy = averageAccuracy / 10.0
+        averagePrecision = averagePrecision / 10.0
+        averageRecall = averageRecall / 10.0
+        averageF1 = averageF1 / 10.0
+        
+        print "Average Accuracy: " + averageAccuracy
+        print "Average Precision: " + averagePrecision
+        print "Average Recall: " + averageRecall
+        print "Average F1: " + averageF1
+        return (averageAccuracy, averagePrecision, averageRecall, averageF1)
 
 
     def classify(self, sText):
@@ -92,10 +153,8 @@ class Bayes_Classifier:
         #print " the total of pos is ", self.pos_total
         #print "the total of neg is ", self.neg_total
 
-        
 
-
-        total_doc = self.pos_doc + self.neg_doc
+        #total_doc = self.pos_doc + self.neg_doc
 
         pos_prob = 0;
         neg_prob = 0;
